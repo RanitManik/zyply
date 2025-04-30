59cf6f15bac96fa5dc966ebcfd404cf37443a5d4
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2025-04-30 22:08:08 +0530

    updated dependencies in frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <h1>Page</h1>;
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="min-h-svh bg-gray-100 hover:bg-gray-200">
+      <Button>Click me</Button>
+    </div>
+  );
 }
